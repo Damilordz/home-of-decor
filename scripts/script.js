b742872a642ec10b9314c61642ab3b52c771b4e0
Damilola Emmanuel <adebowaledami97@gmail.com>
2024-07-27 18:42:46 +0100

    Add contact us page

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const navBar = document.getElementById("nav-bar");
 const closeNav = document.getElementById("close-nav");
 const navMenu = document.getElementById("nav-menu");
 const navLinks = document.querySelectorAll("#nav-links a");
+const myAccount = document.querySelector(".my-account a");
 
 // Add event listener to open the navigation menu when the navBar is clicked
 navBar.addEventListener("click", () => {
@@ -18,8 +19,14 @@ closeNav.addEventListener("click", () => {
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
     // Remove 'active-menu' class from all links
-    navLinks.forEach(link => link.classList.remove("active-menu"));
+    navLinks.forEach((link) => link.classList.remove("active-menu"));
     // Add 'active-menu' class to the clicked link
     navLink.classList.add("active-menu");
   });
+});
+
+// Add event listener to myAccount link
+myAccount.addEventListener("click", () => {
+  // Remove 'active-menu' class from all links
+  navLinks.forEach((link) => link.classList.remove("active-menu"));
 });
