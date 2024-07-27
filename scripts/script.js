7a74cc991744911c0aa8f0044681515e1b96b02f
Damilola Emmanuel <adebowaledami97@gmail.com>
2024-07-27 14:03:54 +0100

    Add sign-in, sign-up, profile and components pages

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// Get references to the DOM elements
+const navBar = document.getElementById("nav-bar");
+const closeNav = document.getElementById("close-nav");
+const navMenu = document.getElementById("nav-menu");
+const navLinks = document.querySelectorAll("#nav-links a");
+
+// Add event listener to open the navigation menu when the navBar is clicked
+navBar.addEventListener("click", () => {
+  navMenu.classList.add("show-nav");
+});
+
+// Add event listener to close the navigation menu when closeNav is clicked
+closeNav.addEventListener("click", () => {
+  navMenu.classList.remove("show-nav");
+});
+
+// Add event listeners to each navigation link
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    // Remove 'active-menu' class from all links
+    navLinks.forEach(link => link.classList.remove("active-menu"));
+    // Add 'active-menu' class to the clicked link
+    navLink.classList.add("active-menu");
+  });
+});
