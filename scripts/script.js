b91a6ed60cf69e6d074bbe5b751fea32d059d488
Temijopelo Jeje <temijeje@gmail.com>
2024-08-04 21:00:20 +0100

    done with the product

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,46 @@ myAccount.addEventListener("click", () => {
   // Remove 'active-menu' class from all links
   navLinks.forEach((link) => link.classList.remove("active-menu"));
 });
+
+// product-page js
+
+const badgeHeart = document.querySelectorAll(".card-item");
+
+badgeHeart.forEach((item) => {
+  const liked = item.querySelector(".heart");
+
+  liked.addEventListener("click", () => {
+    liked.classList.remove("fa-regular");
+    liked.classList.add("fa-solid");
+
+    liked.classList.toggle("liked");
+  });
+});
+
+// sidebar js
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all filter heading elements
+  const filterHeadings = document.querySelectorAll(".filter_heading");
+
+  // Iterate over each filter heading
+  filterHeadings.forEach(function (filterHeading) {
+    // Find the associated list and icon within the current filter heading
+    const filterList = filterHeading.nextElementSibling;
+    const toggleIcon = filterHeading.querySelector(".toggle-icon");
+
+    // Set up the click event listener for each heading
+    filterHeading.addEventListener("click", function () {
+      // Toggle the display of the list
+      if (
+        filterList.style.display === "none" ||
+        filterList.style.display === ""
+      ) {
+        filterList.style.display = "flex";
+        toggleIcon.classList.add("active");
+      } else {
+        filterList.style.display = "none";
+        toggleIcon.classList.remove("active");
+      }
+    });
+  });
+});
